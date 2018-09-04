e9f9e590114468bddf93734f41a713f0c03be969
Julian <36549405+julian-klinger@users.noreply.github.com>
2018-09-04 10:47:41 +0200

    - login bootstrapified

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export class Login extends Component {
 	constructor() {
@@ -37,13 +37,28 @@ export class Login extends Component {
 
 	render() {
 		return(
-			<form onSubmit={this.handleSubmit}>
-			  <span>E-Mail:</span>
-			  <input type="text" name="e-mail" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})}/>
-			  <span>Password:</span>
-			  <input type="text" name="password" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})}/>
-			  <Button type="submit" color="primary">Submit</Button>
-			</form>
+			<Form onSubmit={this.handleSubmit}>
+				<h2>Login zum Admin-Dashboard</h2>
+
+        <FormGroup row>
+        	<Col xs={{ size: 10, offset: 1 }} md={{ size: 4, offset: 4 }}>
+	          <Label for="exampleEmail">Email</Label>
+	          <Input type="text" name="e-mail" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} placeholder="E-Mail-Adresse" />
+        	</Col>
+        </FormGroup>
+
+        <FormGroup row>
+        	<Col xs={{ size: 10, offset: 1 }} md={{ size: 4, offset: 4 }}>
+	          <Label for="examplePassword">Password</Label>
+	          <Input  type="text" name="password" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} placeholder="Passwort" />
+        	</Col>
+        </FormGroup>
+
+        <FormGroup>
+	        <Button type="submit" color="primary">Submit</Button>
+				</FormGroup>
+
+		  </Form>
 		)
 	}
 
