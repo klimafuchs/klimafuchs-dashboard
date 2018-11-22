44eff4f8653c08554e9f31a5054c982b42090c91
Julian <julianmklinger@gmail.com>
2018-11-22 16:48:12 +0100

    - themenwoches query

--- src/components/planning.js
+++ src/components/planning.js
@@ -1,180 +1,67 @@
 import React from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo';
+import { Row, Card, CardTitle, CardText, CardColumns, Container, Col } from 'reactstrap';
 
-// fake data generator
-const getItems = (count, offset = 0) =>
-    Array.from({ length: count }, (v, k) => k).map(k => ({
-        id: `item-${k + offset}`,
-        content: `item ${k + offset}`
-    }));
-
-// a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    return result;
-};
-
-/**
- * Moves an item from one list to another list.
- */
-const move = (source, destination, droppableSource, droppableDestination) => {
-    const sourceClone = Array.from(source);
-    const destClone = Array.from(destination);
-    const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-    destClone.splice(droppableDestination.index, 0, removed);
-
-    const result = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
-
-    return result;
-};
-
-const grid = 8;
-
-const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: 'none',
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
-
-    // change background colour if dragging
-    background: isDragging ? 'lightgreen' : 'grey',
-
-    // styles we need to apply on draggables
-    ...draggableStyle
-});
-
-const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : 'lightgrey',
-    padding: grid,
-    width: 250
-});
-
-export class Planning extends React.Component {	
-    state = {
-        items: getItems(10),
-        selected: getItems(5, 10)
-    };
-
-    /**
-     * A semi-generic way to handle multiple lists. Matches
-     * the IDs of the droppable container to the names of the
-     * source arrays stored in the state.
-     */
-    id2List = {
-        droppable: 'items',
-        droppable2: 'selected'
-    };
-
-    getList = id => this.state[this.id2List[id]];
-
-    onDragEnd = result => {
-        const { source, destination } = result;
-
-        // dropped outside the list
-        if (!destination) {
-            return;
-        }
-
-        if (source.droppableId === destination.droppableId) {
-            const items = reorder(
-                this.getList(source.droppableId),
-                source.index,
-                destination.index
-            );
-
-            let state = { items };
-
-            if (source.droppableId === 'droppable2') {
-                state = { selected: items };
+const THEMENWOCHES_LIST = gql`
+	query {
+        themenwoches {            
+            title
+            content
+            createdAt
+            updatedAt
+            oberthema {
+            name
             }
-
-            this.setState(state);
-        } else {
-            const result = move(
-                this.getList(source.droppableId),
-                this.getList(destination.droppableId),
-                source,
-                destination
-            );
-
-            this.setState({
-                items: result.droppable,
-                selected: result.droppable2
-            });
         }
-    };
+    }`
 
-    // Normally you would want to split things out into separate components.
-    // But in this example everything is just done in one place for simplicity
-    render() {
-        return (
-            <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable">
-                    {(provided, snapshot) => (
-                        <div
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}>
-                            {this.state.items.map((item, index) => (
-                                <Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            style={getItemStyle(
-                                                snapshot.isDragging,
-                                                provided.draggableProps.style
-                                            )}>
-                                            {item.content}
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-                <Droppable droppableId="droppable2">
-                    {(provided, snapshot) => (
-                        <div
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}>
-                            {this.state.selected.map((item, index) => (
-                                <Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}>
-                                    {(provided, snapshot) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            style={getItemStyle(
-                                                snapshot.isDragging,
-                                                provided.draggableProps.style
-                                            )}>
-                                            {item.content}
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
-        );
-    }
+const SEASONS_LIST = gql`
+	query {
+        seasons {
+            id
+            title
+            startDate
+            startOffsetDate
+            endDate    
+        }
+    }`
+
+export class Planning extends React.Component {
+
+	render() {
+
+		return(
+			<Query query={THEMENWOCHES_LIST}>
+				{({ loading, error, data, refetch }) => {
+					if (loading) return <div>its loading</div>;
+					if (error) return <div>${error.message}</div>;
+					return (
+                        <Container>
+                            <Row>
+                                <Col xs="8">
+{/* put in separate component TODO  {data.seasons.map(season => (
+                                        <Card className="my-1" key={season.title} body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                                            <CardTitle>{season.title}</CardTitle>
+                                            <CardText className="small">{season.startDate}</CardText>
+                                        </Card>
+                                    ))}                                 */}
+                                </Col>
+                                <Col xs="4">
+                                <h2>Themenwochen</h2>
+                                    {data.themenwoches.map(themenwoche => (
+                                        <Card className="my-1" key={themenwoche.title} body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                                            <CardTitle>{themenwoche.title}</CardTitle>
+                                            <CardText className="small">{themenwoche.content}</CardText>
+                                        </Card>
+                                    ))}
+                                </Col>
+                            </Row>
+                        </Container>
+
+                    )}}
+
+			</Query>
+		)
+	}
 }
-
-
-export default Planning
