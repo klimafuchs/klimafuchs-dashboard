282fc040c71071966a21aa8ea2bf56280416cebc
Julian <julianmklinger@gmail.com>
2018-11-30 13:45:16 +0100

    test

--- src/components/stats.js
+++ src/components/stats.js
@@ -4,7 +4,7 @@ export class Stats extends React.Component {
 	render() {
 		return (
 			<div>
-				Another commit test for the server
+				Another commit test for the server asdfasdf
 			</div>
 		)
 	}
