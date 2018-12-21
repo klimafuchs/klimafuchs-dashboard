39507676b89879bd69414ad976d33906034a45cf
Julian <julianmklinger@gmail.com>
2018-12-21 16:25:32 +0100

    fix

--- src/components/addSeasonPlan.js
+++ src/components/addSeasonPlan.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag'
 import { Query, Mutation } from 'react-apollo';
 import { Col, Row, Input, Button, Form, Label } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const ADD_SEASONPLAN = gql`
 	mutation addSeasonPlan($sID:Int!, $tID:String!, $pos:Int!, $duration:Int!) {
@@ -47,137 +47,143 @@ const THEMENWOCHES_LIST = gql`
 
 export class AddSeasonPlan extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-		};
-	}
+constructor(props) {
+	super(props);
+	this.state = {
+	};
+}
 
-	DaysInSeconds = (days) => {
-		let result = Math.round(days * 86400)
-		return result
-	}
+DaysInSeconds = (days) => {
+	let result = Math.round(days * 86400)
+	return result
+}
 
-	render() {
-		return (
+IsValid = () => {
+	console.log(this.state.position);
+	console.log(this.state.duration);
 
-			<Mutation mutation={ADD_SEASONPLAN}>
-				{(updateSeasonPlan, { data }) => (
-					<Form onSubmit={e => {
-						e.preventDefault();
-						updateSeasonPlan({
-							variables: { tID: this.state.title, sID: this.props.season.id, duration: this.DaysInSeconds(this.state.duration), pos: this.state.position },
-							refetchQueries: [{ query: SEASON_PLANS }]
-						})
+}
 
-					}}>
+render() {
+	return (
 
-						<div className="small font-italic text-left ml-2" >
-							<span>Add a Themenwoche</span>
-						</div>
+		<Mutation mutation={ADD_SEASONPLAN}>
+			{(updateSeasonPlan, { data }) => (
+				<Form onSubmit={e => {
+					e.preventDefault();
+					updateSeasonPlan({
+						variables: { tID: this.state.title, sID: this.props.season.id, duration: this.DaysInSeconds(this.state.duration), pos: this.state.position },
+						refetchQueries: [{ query: SEASON_PLANS }]
+					})
+				}}>
 
-						<Row className="bg-light p-1 shadow-sm mx-1">
+					<div className="small font-italic text-left ml-2" >
+						<span>Add a Themenwoche</span>
+					</div>
 
-							<Col>
-								<Downshift
-									onChange={selection => this.setState({ title: selection.title })}
-									itemToString={item => (item ? item.title : '')}
-								>
-									{({
-										getInputProps,
-										getItemProps,
-										getLabelProps,
-										getMenuProps,
-										isOpen,
-										inputValue,
-										highlightedIndex,
-										selectedItem,
-									}) => (
-											<div>
-												{/* <label {...getLabelProps()}>Themenwoche</label> */}
-												<Input className="px-2 my-2" placeholder="start typing..." {...getInputProps()} />
+					<Row className="bg-light p-1 shadow-sm mx-1">
 
-												<ul {...getMenuProps()} className="p-0 pr-4 w-100">
-													{isOpen
+						<Col>
+							<Downshift
+								onChange={selection => this.setState({ title: selection.title })}
+								itemToString={item => (item ? item.title : '')}
+							>
+								{({
+									getInputProps,
+									getItemProps,
+									getLabelProps,
+									getMenuProps,
+									isOpen,
+									inputValue,
+									highlightedIndex,
+									selectedItem,
+								}) => (
+										<div>
+											{/* <label {...getLabelProps()}>Themenwoche</label> */}
+											<Input className="px-2 my-2" placeholder="start typing..." {...getInputProps()} />
 
-														?
+											<ul {...getMenuProps()} className="p-0 pr-4 w-100">
+												{isOpen
 
-														<Query query={THEMENWOCHES_LIST}>
+													?
 
-															{({ loading, error, data, refetch }) => {
-																if (loading) return <div>its loading</div>;
-																if (error) return <div>${error.message}</div>;
-																return (
-																	<div>
-																		{
-																			data.themenwoches
-																				.filter(item => !inputValue || item.title.toLowerCase().includes(inputValue.toLowerCase()))
-																				.map((item, index) => (
+													<Query query={THEMENWOCHES_LIST}>
 
-																					<li className="p-1 text-left"
-																						{...getItemProps({
-																							key: item.title,
-																							index,
-																							item,
-																							style: {
-																								color:
-																									highlightedIndex === index ? '#007BFF' : 'black',
-																								cursor: 'pointer',
-																								listStyleType: 'none'
-																							},
-																						})}
-																					>
-																						{item.title}
-																					</li>
-																				))}
-																	</div>
-																)
-															}}
-														</Query>
-														: null}
-												</ul>
+														{({ loading, error, data, refetch }) => {
+															if (loading) return <div>its loading</div>;
+															if (error) return <div>${error.message}</div>;
+															return (
+																<div>
+																	{
+																		data.themenwoches
+																			.filter(item => !inputValue || item.title.toLowerCase().includes(inputValue.toLowerCase()))
+																			.map((item, index) => (
 
-											</div>
-										)}
-								</Downshift>
-							</Col>
+																				<li className="p-1 text-left"
+																					{...getItemProps({
+																						key: item.title,
+																						index,
+																						item,
+																						style: {
+																							color:
+																								highlightedIndex === index ? '#007BFF' : 'black',
+																							cursor: 'pointer',
+																							listStyleType: 'none'
+																						},
+																					})}
+																				>
+																					{item.title}
+																				</li>
+																			))}
+																</div>
+															)
+														}}
+													</Query>
+													: null}
+											</ul>
 
-							<Col>
-								<Input className="px-2 my-2"
-									placeholder="duration in days"
-									type="number" min="0"
-									style={{ textAlign: 'center' }}
-									onChange={(e) => this.setState({ duration: e.target.value })}>
-								</Input>
-							</Col>
+										</div>
+									)}
+							</Downshift>
+						</Col>
 
-							<Col>
-								<Input className="px-2 my-2"
-									placeholder="position in season"
-									type="number" min="1"
-									style={{ textAlign: 'center' }}
-									onChange={(e) => this.setState({ position: e.target.value })}>
-								</Input>
-							</Col>
+						<Col>
+							<Input className="px-2 my-2"
+								placeholder="duration in days"
+								type="number" min="0"
+								style={{ textAlign: 'center' }}
+								onChange={(e) => this.setState({ duration: e.target.value })}>
+							</Input>
+						</Col>
 
-							<button type="submit" className="btn btn-link text-primary">
-								<FontAwesomeIcon
-									style={{ fontSize: '16px', cursor: "pointer" }}
-									icon={faPlus}
-								>
-								</FontAwesomeIcon>
-							</button>
+						<Col>
+							<Input className="px-2 my-2"
+								placeholder="position in season"
+								type="number" min="1"
+								style={{ textAlign: 'center' }}
+								onChange={(e) => this.setState({ position: e.target.value })}>
+							</Input>
 
-						</Row>
+						</Col>
 
-					</Form>
-				)
-				}
-			</Mutation>
+						<button type="submit" className="btn btn-link text-primary">
+							<FontAwesomeIcon
+								style={{ fontSize: '16px', cursor: "pointer" }}
+								icon={faPlus}
+							>
+							</FontAwesomeIcon>
+						</button>
+
+					</Row>
+
+				</Form>
+			)
+			}
+		</Mutation>
 
 
 
 
-		)
-	}
+	)
+}
 }
