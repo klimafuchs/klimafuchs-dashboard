a8dbfb5bc9babbae40050794548b60f15d5304c3
Julian <julianmklinger@gmail.com>
2018-12-21 10:15:49 +0100

    currentSeason

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo';
-import { CardTitle, CardText, Jumbotron } from 'reactstrap';
+import { CardTitle, CardText, Jumbotron, Row, Col } from 'reactstrap';
 import Time from 'react-time-format';
 
 const CURRENT_SEASON = gql`
@@ -14,13 +14,35 @@ const CURRENT_SEASON = gql`
 		title
 			seasonPlan {
 				id
-			}
-		
-	}
+			}		
+		}
 	}
  `
 
+const SEASON_PLANS = gql`
+	query seasonPlans { 
+		seasonPlans {
+			id
+				themenwoche {
+					title
+				}
+			duration
+			position
+			season {
+				id
+			}
+		}
+	}
+`
+
+
 export class Stats extends React.Component {
+
+	secondsInDays = (seconds) => {
+		let result = Math.round(seconds / 86400)
+		return ((result === 1) ? result + " day" : result + " days")
+	}
+
 	render() {
 		return (
 			<div>
@@ -29,11 +51,52 @@ export class Stats extends React.Component {
 					{({ loading, error, data, refetch }) => {
 						if (loading) return <div>its loading</div>;
 						if (error) return <div>${error.message}</div>;
+						let CurrentSeasonID = data.currentSeason.id
 						return (
 							<div>
-								{
-									data.currentSeason.title
-								}
+
+								<Jumbotron className="shadow-sm my-1">
+									<CardTitle>
+										{data.currentSeason.title}
+									</CardTitle>
+									<CardText className="small">
+										Season starts at: <Time value={data.currentSeason.startDate} format="DD.MM.YYYY"></Time> |
+                        First Topicweek starts at: <Time value={data.currentSeason.startOffsetDate} format="DD.MM.YYYY"></Time> |
+                        Season ends at: <Time value={data.currentSeason.endDate} format="DD.MM.YYYY"></Time> |
+                        ID: {data.currentSeason.id}
+									</CardText>
+									<Query query={SEASON_PLANS}>
+										{({ loading, error, data, refetch }) => {
+											if (loading) return <div>its loading</div>;
+											if (error) return <div>${error.message}</div>;
+											return (
+												<div>
+													{
+														data.seasonPlans.map(seasonPlan => (
+															<div key={seasonPlan.id}>
+																{CurrentSeasonID === seasonPlan.season.id ?
+																	<Row className="bg-light p-1 shadow-sm my-1">
+																		<Col>
+																			{seasonPlan.themenwoche.title}
+																		</Col>
+																		<Col>
+																			{this.secondsInDays(seasonPlan.duration)}
+																		</Col>
+																		<Col>
+																			Position: {seasonPlan.position}
+																		</Col>
+																	</Row>
+																	: null
+																}
+															</div>
+														))
+													}
+												</div>
+											)
+										}}
+									</Query>
+								</Jumbotron>
+
 							</div>
 						)
 					}}
