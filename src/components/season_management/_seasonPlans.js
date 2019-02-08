5a9845a17ff93b85c3caa9cf14993e69b0a01659
Julian <julianmklinger@gmail.com>
2019-02-08 15:14:06 +0100

    more nice (not working) functionality...

--- src/components/season_management/_seasonPlans.js
+++ src/components/season_management/_seasonPlans.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag'
 import { Row, Col } from 'reactstrap'
 import { Query, Mutation } from 'react-apollo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import confirm from 'reactstrap-confirm'
+import { faTrash, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import confirm from 'reactstrap-confirm';
 
 const SEASON_PLANS = gql`
 	query seasonPlans { 
@@ -22,8 +22,22 @@ const SEASON_PLANS = gql`
 	}
 `
 
+const UPDATE_SEASONPLAN = gql`
+	mutation updateSeasonPlan($spId:Int!, $pos:Int!, $duration:Int!) {
+		updateSeasonPlan(seasonPlan:{
+			seasonPlanId:$spId,
+			position: $pos,
+			duration: $duration
+		}) {
+			id,
+			position,
+			duration
+		}
+	} 
+`
+
 const REMOVE_SEASON_PLAN = gql`
-	mutation rSP($spId:Int!){
+	mutation removeSeasonPlan($spId:Int!){
 		removeSeasonPlan(seasonPlanId:$spId){
 			duration
 		}
@@ -31,6 +45,15 @@ const REMOVE_SEASON_PLAN = gql`
 `
 
 export class SeasonPlans extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			spID: "",
+			duration: "",
+			position: ""
+		};
+	}
 
 	secondsInDays = (seconds) => {
 		let result = Math.round(seconds / 86400)
@@ -62,15 +85,59 @@ export class SeasonPlans extends React.Component {
 															</span>
 														</Col>
 														<Col className="d-flex align-items-center justify-content-center">
+															<button type="submit" className="btn btn-link text-primary">
+																<FontAwesomeIcon
+																	style={{ fontSize: '16px', cursor: "pointer" }}
+																	icon={faMinus}
+																>
+																</FontAwesomeIcon>
+															</button>
+
 															{this.secondsInDays(seasonPlan.duration)}
+
+															<Mutation mutation={UPDATE_SEASONPLAN}>
+																{(updateSeasonPlan, { data }) => (
+																	<button
+																		className="btn btn-link text-warning"
+																		onClick={
+																			console.log("test")
+																		}>
+																		<FontAwesomeIcon
+																			style={{ fontSize: '16px', cursor: "pointer" }}
+																			icon={faPlus}
+																		>
+																		</FontAwesomeIcon>
+																	</button>
+																)}
+															</Mutation>
+
 														</Col>
+
 														<Col className="d-flex align-items-center justify-content-center ">
-															{seasonPlan.position}
+															<button type="submit" className="btn btn-link text-primary">
+																<FontAwesomeIcon
+																	style={{ fontSize: '16px', cursor: "pointer" }}
+																	icon={faMinus}
+																>
+																</FontAwesomeIcon>
+															</button>
+
+															<span>
+																{seasonPlan.position}
+															</span>
+
+															<button type="submit" className="btn btn-link text-primary">
+																<FontAwesomeIcon
+																	style={{ fontSize: '16px', cursor: "pointer" }}
+																	icon={faPlus}
+																>
+																</FontAwesomeIcon>
+															</button>
 														</Col>
+
 														<Mutation mutation={REMOVE_SEASON_PLAN}>
 															{(deleteSeasonPlan, { data, _ }) => (
 																<button
-																	type="submit"
 																	className="btn btn-link text-primary"
 																	onClick={async e => {
 																		if (await confirm()) {
