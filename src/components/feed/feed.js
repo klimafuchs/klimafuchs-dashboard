74d4fe69e3cff94eeba82362fe7a109da2a8328a
Julian <julianmklinger@gmail.com>
2019-02-14 17:30:52 +0100

    stuff

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import {Mutation, Query} from 'react-apollo';
-import {Card, CardImg, CardText, CardTitle, Col, Pagination, PaginationItem, PaginationLink, Row} from 'reactstrap';
-import {AddPost} from './_addpost';
-import {Comments} from './_comments'
+import { Mutation, Query } from 'react-apollo';
+import { Card, CardImg, CardText, CardTitle, Col, Pagination, PaginationItem, PaginationLink, Row } from 'reactstrap';
+import { AddPost } from './_addpost';
+import { Comments } from './_comments'
 import Time from 'react-time-format';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import Alert from 'react-s-alert';
 import confirm from 'reactstrap-confirm';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const FEED = gql`
     query FeedPage($page: ConnectionArgs!) {
@@ -59,153 +59,151 @@ const img_url = process.env.REACT_APP_IMG_URL
 
 export class Feed extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.pageSize = 3
-    }
+  constructor(props) {
+    super(props)
+    this.pageSize = 2
+  }
 
 
-    render() {
-        return (
-            <Query
-                query={FEED}
-                variables={{page: {first: this.pageSize}}}
-                fetchPolicy="cache-and-network"
-            >
-                {({loading, error, data, refetch, fetchMore}) => {
-                    if (loading) return <div>its loading</div>;
-                    if (error) return <div>${error.message}</div>;
-                    return (
+  render() {
+    return (
+      <Query
+        query={FEED}
+        variables={{ page: { first: this.pageSize } }}
+        fetchPolicy="cache-and-network"
+      >
+        {({ loading, error, data, refetch, fetchMore }) => {
+          if (loading) return <div>its loading</div>;
+          if (error) return <div>${error.message}</div>;
+          return (
 
-                        <div>
-                            <Row>
-                                <Col xs="12">
-                                    <AddPost/>
-                                </Col>
-                            </Row>
+            <div>
+              <Row>
+                <Col xs="12">
+                  <AddPost />
+                </Col>
+              </Row>
 
-                            <TransitionGroup className="todo-list">
+              <TransitionGroup className="todo-list">
 
-                                {data.paginatedPosts.page.edges
-                                    .slice(0)
-                                    .sort((a, b) => {
-                                        return (new Date(b.dateCreated).getTime()) - (new Date(a.dateCreated).getTime());
-                                    })
-                                    .sort((a, b) => {
-                                        return (b.node.isPinned) - (a.node.isPinned);
-                                    })
-                                    .map(post => (
+                {data.paginatedPosts.page.edges
+                  .slice(0)
+                  .sort((a, b) => {
+                    return (new Date(b.dateCreated).getTime()) - (new Date(a.dateCreated).getTime());
+                  })
+                  .sort((a, b) => {
+                    return (b.node.isPinned) - (a.node.isPinned);
+                  })
+                  .map(post => (
 
-                                        <CSSTransition
-                                            key={post.node.id}
-                                            timeout={500}
-                                            classNames="move"
-                                        >
-                                            <Row className="align-items-end my-1" key={post.node.id}>
+                    <CSSTransition
+                      key={post.node.id}
+                      timeout={500}
+                      classNames="move"
+                    >
+                      <Row className="align-items-end my-1" key={post.node.id}>
 
-                                                <Col xs="12" lg="12">
-                                                    <Card
-                                                        body
-                                                        className={`text-left pb-1 my-1 post ${post.node.isPinned ? "border border-primary" : null}`}
-                                                    >
+                        <Col xs="12" lg="12">
+                          <Card
+                            body
+                            className={`text-left pb-1 my-1 post ${post.node.isPinned ? "border border-primary" : null}`}
+                          >
 
-                                                        <Mutation mutation={DELETE_POST}>
-                                                            {(deletePost, {data, _}) => (
-                                                                <div className="position-absolute"
-                                                                     style={{right: '5px', top: '5px'}}>
-                                                                    <FontAwesomeIcon
-                                                                        className="text-primary"
-                                                                        style={{fontSize: '16px', cursor: "pointer"}}
-                                                                        icon={faTimes}
-                                                                        onClick={async e => {
-                                                                            if (await confirm()) {
-                                                                                await deletePost({variables: {id: post.node.id}}).catch(error => Alert.error(`${error}`, {
-                                                                                    position: 'top',
-                                                                                    effect: 'slide',
-                                                                                    timeout: 3000
-                                                                                }))
-                                                                                // wait for the delete mutation to return, otherwise the deleted post will still be in the db when refetch() runs
-                                                                                refetch(); // refetch belongs to the surrounding FEED query
-                                                                            }
-                                                                        }}>
-                                                                    </FontAwesomeIcon>
-                                                                </div>
-                                                            )}
-                                                        </Mutation>
+                            <Mutation mutation={DELETE_POST}>
+                              {(deletePost, { data, _ }) => (
+                                <div className="position-absolute"
+                                  style={{ right: '5px', top: '5px' }}>
+                                  <FontAwesomeIcon
+                                    className="text-primary"
+                                    style={{ fontSize: '16px', cursor: "pointer" }}
+                                    icon={faTimes}
+                                    onClick={async e => {
+                                      if (await confirm()) {
+                                        await deletePost({ variables: { id: post.node.id } }).catch(error => Alert.error(`${error}`, {
+                                          position: 'top',
+                                          effect: 'slide',
+                                          timeout: 3000
+                                        }))
+                                        // wait for the delete mutation to return, otherwise the deleted post will still be in the db when refetch() runs
+                                        refetch(); // refetch belongs to the surrounding FEED query
+                                      }
+                                    }}>
+                                  </FontAwesomeIcon>
+                                </div>
+                              )}
+                            </Mutation>
 
-                                                        {post.node.isPinned ? <span
-                                                            className="text-center text-primary font-italic font-weight-bold">Pinned Content</span> : null}
+                            {post.node.isPinned ? <span
+                              className="text-center text-primary font-italic font-weight-bold">Pinned Content</span> : null}
 
-                                                        {post.node.image == null ? null : <CardImg top width="100%"
-                                                                                                   src={`${img_url}/${post.node.image.filename}`}
-                                                                                                   alt="Card image cap"/>}
+                            {post.node.image == null ? null : <CardImg top width="100%"
+                              src={`${img_url}/${post.node.image.filename}`}
+                              alt="Card image cap" />}
 
-                                                        <CardTitle className="mt-3">
-                                                            Title: {post.node.title}
-                                                        </CardTitle>
-                                                        <CardText>
-                                                            Text: {post.node.body}
-                                                        </CardText>
-                                                        <span className="annotation font-italic">
-															Author: {post.node.author.screenName},
+                            <CardTitle className="mt-3">
+                              Title: {post.node.title}
+                            </CardTitle>
+                            <CardText>
+                              Text: {post.node.body}
+                            </CardText>
+                            <span className="annotation font-italic">
+                              Author: {post.node.author.screenName},
 															User-ID: {post.node.author.id},
 															Post-ID: {post.node.id},
 															IsPinned?: {String(post.node.isPinned)},
 															<Time value={post.node.dateCreated}
-                                                                  format="DD.MM.YYYY hh:mm:ss"></Time>
-														</span>
-                                                    </Card>
-                                                </Col>
+                                format="DD.MM.YYYY hh:mm:ss"></Time>
+                            </span>
+                          </Card>
+                        </Col>
 
-                                                <Comments passed_refetch={() => refetch(FEED)}
-                                                          comments={post.node.comments}/>
+                        <Comments passed_refetch={() => refetch(FEED)}
+                          comments={post.node.comments} />
 
 
-                                            </Row>
-                                        </CSSTransition>
-                                    ))
-                                }
-                            </TransitionGroup>
-
-                            <Pagination className="mt-5" aria-label="Page navigation example">
-
-                                <PaginationItem>
-                                    <PaginationLink previous href="#"/>
-                                </PaginationItem>
-                                <PaginationItem>
-                                    <PaginationLink
-                                        onClick={() => {
-                                            const lastCursor = data.paginatedPosts.page.edges[data.paginatedPosts.page.edges.length - 1].cursor;
-                                            fetchMore({
-                                                variables: {
-                                                    page: {
-                                                        first: this.pageSize,
-                                                        after: lastCursor
-                                                    }
-                                                },
-                                                updateQuery: (prev, {fetchMoreResult}) => {
-                                                    if (!fetchMoreResult) return prev;
-                                                    if (fetchMoreResult.paginatedPosts.page.edges.length === 0) {
-                                                        console.log("no more data");
-                                                        this.setState({endReached: true})
-                                                    }
-                                                    return Object.assign(data.paginatedPosts.page, prev, {
-                                                        edges: [...prev.paginatedPosts.page.edges, ...fetchMoreResult.paginatedPosts.page.edges]
-                                                    });
-                                                }
-                                            })
-
-                                        }}> more
-                                    </PaginationLink>
-                                </PaginationItem>
-                            </Pagination>
-
-                        </div>
-                    )
+                      </Row>
+                    </CSSTransition>
+                  ))
                 }
-                }
-            </Query>
-        )
-    }
+              </TransitionGroup>
+
+              <Pagination className="mt-5" aria-label="Page navigation example">
+
+                <PaginationItem>
+                  <PaginationLink
+                    onClick={() => {
+                      const lastCursor = data.paginatedPosts.page.edges[data.paginatedPosts.page.edges.length - 1].cursor;
+                      fetchMore({
+                        variables: {
+                          page: {
+                            first: this.pageSize,
+                            after: lastCursor
+                          }
+                        },
+                        updateQuery: (prev, { fetchMoreResult }) => {
+                          if (!fetchMoreResult) return prev;
+                          if (fetchMoreResult.paginatedPosts.page.edges.length === 0) {
+                            Alert.warning("No more posts", {
+                              position: 'top', timeout: 3000, effect: 'slide'
+                            });
+                            this.setState({ endReached: true })
+                          }
+                          return Object.assign(data.paginatedPosts.page, prev, {
+                            edges: [...prev.paginatedPosts.page.edges, ...fetchMoreResult.paginatedPosts.page.edges]
+                          })
+                        }
+                      })
+                    }}> show 10 more posts
+                  </PaginationLink>
+                </PaginationItem>
+              </Pagination>
+
+            </div>
+          )
+        }
+        }
+      </Query>
+    )
+  }
 
 }
