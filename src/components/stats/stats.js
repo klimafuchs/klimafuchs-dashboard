8f33716809f45ea3fce5164ffd9bbb397d88348f
julian <julianmklinger@gmail.com>
2019-03-15 12:36:14 +0100

    fix for undefined current season

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -34,7 +34,7 @@ export class Stats extends React.Component {
 				<Query query={CURRENT_SEASON}>
 					{({ loading, error, data, refetch }) => {
 						if (loading) return <div><img className="col-1 pt-5 mt-5" src="./load-spinner.gif" alt="Klimafuchs-Logo" /></div>;
-						if(data.currentSeason == null) return <div>no current season existing</div>
+						if(data == undefined) return <div>no current season existing</div>
 						if (error) return <div>${error.message}</div>;
 						return (
 							<div>
